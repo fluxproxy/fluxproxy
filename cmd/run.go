d6bc4f74aee77b5be789cfacddafd4d24a480777
chenxxpro <chenxxpro@hotmail.com>
2024-06-23 13:07:05 +0800

    feat: config

--- cmd/run.go
+++ cmd/run.go
@@ -6,14 +6,27 @@ import (
 	"fluxway/proxy"
 	"fmt"
 	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
 
 func runCommand(ctx *cli.Context) error {
+	confpath := "conf.yml"
+	if ctx.NArg() > 0 {
+		confpath = ctx.Args().Get(0)
+	}
+	// Configuration
 	var k = koanf.NewWithConf(koanf.Conf{
 		Delim:       ".",
 		StrictMerge: true,
 	})
+	logrus.Infof("load config file: %s", confpath)
+	if err := k.Load(file.Provider(confpath), yaml.Parser()); err != nil {
+		return fmt.Errorf("load config file %s: %w", confpath, err)
+	}
+	// Instance
 	inst := fluxway.NewInstance(proxy.ContextWithConfig(ctx.Context, k))
 	if err := inst.Start(); err != nil {
 		return fmt.Errorf("instance start: %w", err)
