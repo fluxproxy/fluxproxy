5f5918b61a1b324fb5998d40640c7fbc7553eeb8
chenxxpro <chenxxpro@hotmail.com>
2024-06-23 23:21:48 +0800

    feat: forward

--- common/types.go
+++ common/types.go
@@ -1,5 +1,31 @@
 package common
 
+// Semaphore is an implementation of semaphore.
+type Semaphore struct {
+	token chan struct{}
+}
+
+// NewSemaphore create a new Semaphore with n permits.
+func NewSemaphore(n int) *Semaphore {
+	s := &Semaphore{
+		token: make(chan struct{}, n),
+	}
+	for i := 0; i < n; i++ {
+		s.token <- struct{}{}
+	}
+	return s
+}
+
+// Wait returns a channel for acquiring a permit.
+func (s *Semaphore) Wait() <-chan struct{} {
+	return s.token
+}
+
+// Signal releases a permit into the semaphore.
+func (s *Semaphore) Signal() {
+	s.token <- struct{}{}
+}
+
 type AddressOptions struct {
 	Address string `yaml:"address"`
 	Port    int    `yaml:"port"`
